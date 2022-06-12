4bcf62dd39076e83605dd31eb019f10baf550230
Ramiro Fadul <ramirofadul@gmail.com>
2022-06-12 13:23:02 +0000

    Finish logging

--- blango/urls.py
+++ blango/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 import blog.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", blog.views.index),
+    path('admin/', admin.site.urls,),
+    path("", blog.views.index, name="index"),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
 ]
